ae57d4e6bb3d97040a124731fd6efec79854b188
Sergio <sergiomattei@outlook.com>
2017-03-16 23:40:29 -0400

    Create temporary ignore_limits for development

--- eduview2/resources.py
+++ eduview2/resources.py
@@ -2,6 +2,7 @@ from flask import Response, request
 from flask_restful import Resource, abort, reqparse
 from pymongo import MongoClient
 from bson.json_util import dumps
+import time
 from . import filters
 
 client = MongoClient()
@@ -12,6 +13,7 @@ graduates = db.graduates
 
 parser = reqparse.RequestParser()
 parser.add_argument('offset', type=int, location='args')
+parser.add_argument('ignore_limits', type=bool, location='args')
 
 class SchoolsResource(Resource):
 	def get(self):
@@ -19,14 +21,25 @@ class SchoolsResource(Resource):
 		args = parser.parse_args()
 		# Query MongoDB
 		data = schools.find({}, {'_id': False})
-		# Set a default limit for the API
-		data = data.limit(5)
+		# Set a default limit for the API.
+		# This is stupid. Fix it
+		# TODO: Fix this hackery
+		if not args['ignore_limits']:
+			data = data.limit(5)
 
 		if args['offset']:
 			data = data.skip(args['offset'])
 
 		# Typecast the list to prevent circular ref errors
 		return filters.to_json(list(data))
+
+class SchoolGPAResource(Resource):
+	def get(self):
+		return "5.0"
+
+class MunicipalityGPAResource(Resource):
+	def get(self):
+		return "5.0"
 
 class SchoolResource(Resource):
 	def get(self, sid):
